b0bc9f470d0d3d12d203d6b55ba8b1e8fba9d56e
Sakshamchawla19 <94595424+Sakshamchawla19@users.noreply.github.com>
2024-07-15 20:04:11 +0530

    Update Product_Price_at_a_Given_Date.sql

--- Leetcode SQL-50/Advanced_Select_and_Joins/Product_Price_at_a_Given_Date.sql
+++ Leetcode SQL-50/Advanced_Select_and_Joins/Product_Price_at_a_Given_Date.sql
@@ -3,7 +3,3 @@ union all
 select product_id , new_price as price from products
 where( product_id , change_date) in (select product_id , max(change_date) as price from products where change_date <=  '2019-08-16' group by product_id)
 
-
-
-
---Rakshit Gupta
